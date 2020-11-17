7841a96f4ca8c5cd8f74ab42d97799f4fee0453f
Ratchou <pelagie.ratchinski@ens-lyon.fr>
2020-11-17 16:03:15 +0000

    Test de la qualité des données modifiées par trimmomatic sur le set de données test

---
+++ test_fastqc_trimmed_data.sh
@@ -0,0 +1,18 @@
+#creating directory for fastqc files
+data_directory="/home/rstudio/data/mydatalocal/data/"
+cd $data_directory
+mkdir -p sra_trimmed_data_test_quality
+cd sra_trimmed_data_test_quality
+
+# fastqc analysis
+
+for fichier in $data_directory/sra_trimmed_data_test/*
+  do
+  fastqc $fichier --outdir . -t 6
+done
+
+#analyse multiqc
+
+multiqc --outdir \
+/home/rstudio/data/mydatalocal/data/sra_trimmed_data_test_quality/ \
+/home/rstudio/data/mydatalocal/data/sra_trimmed_data_test_quality/
